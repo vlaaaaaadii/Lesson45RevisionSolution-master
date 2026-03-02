03317181ceb26188ebf371029c49f55856ecf580
Gimbol4 <Shishkanov.yd@gmail.com>
2026-03-02 17:07:35 +0300

    Add project files.

---
+++ Task01Project/logic.h
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+#include <string>
+
+#define DEFAULT_SIZE 100
+
+using namespace std;
+
+int count_nonzero_elements(int array[DEFAULT_SIZE], int size);
+void print(int array[DEFAULT_SIZE], int size, string name, int expected);
+string convert(int array[DEFAULT_SIZE], int size);
